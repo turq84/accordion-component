fe63de0be3ba0c21ed6eeaeb0b453be5db188ae1
Kayla Gordon <turq84@users.noreply.github.com>
2024-02-26 19:34:40 +0200

    Initial commit

---
+++ src/components/uikit/index.ts
@@ -0,0 +1,5 @@
+export { default as Content } from './Content';
+export { default as Icon } from './Icon';
+export { default as Spacing } from './Spacing';
+export { default as Stack } from './Stack';
+export { default as Text } from './Text';
